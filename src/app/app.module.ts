17354c383738525a9742ac578ae4b1bac3abe379
Delache <delache33@gmail.com>
2019-10-15 19:40:47 +0200

    "Quete router"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
+import { Routes, RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
@@ -8,6 +10,11 @@ import { UserProfileComponent } from './user-profile/user-profile.component';
 import { FilmsComponent } from './films/films.component';
 import { HoverItDirective } from './hover-it.directive';
 
+const appRoutes: Routes = [
+  {path: 'sign-up', component: SignUpComponent},
+  {path: 'user-profile', component: UserProfileComponent },
+  {path: '', component: SignUpComponent}
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,6 +27,7 @@ import { HoverItDirective } from './hover-it.directive';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
