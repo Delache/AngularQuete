2671a8d1e55ffde4cafc8e1a2548e27a268edb51
Delache <delache33@gmail.com>
2019-10-17 11:41:35 +0200

    "formulaireQuete"

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Form } from '../app.form';
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent implements OnInit {
-  email: string ;
-  saisie = 'Entrer votre Email';
+
+  model: Form = new Form();
   constructor() { }
 
   ngOnInit() {
   }
-
+  onSubmit() {
+    console.log('message envoyé');
+  }
 }
